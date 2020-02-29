c8bba2e8bae5c7d8ed4124143ef35665649dcf18
Nicole Yang <yoyotpe@hotmail.com>
2020-02-29 16:58:01 +0800

    centralized api

--- src/services/MovieApi.js
+++ src/services/MovieApi.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 export default {
+  fetchMovies () {
+    return axios.get('https://api.themoviedb.org/3/discover/movie?api_key=8e70e008335b9cf0fe44b80e1c509a0d&sort_by=popularity.desc')
+      .then(response => {
+        return response.data.results;
+      })
+  },
   fetchSingleMovie (title) {
     return axios.get('https://api.themoviedb.org/3/search/movie?api_key=8e70e008335b9cf0fe44b80e1c509a0d&query=' + title)
       .then(response => {
-        return response.data.results
+        return response.data.results;
       })
   }
 }
